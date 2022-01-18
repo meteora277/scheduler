7e775f741ffda187fc3747d5e423ec6775709ca1
meteora277 <justin.s.diaz@gmail.com>
2022-01-18 16:22:44 -0500

    create Button functional component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,19 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  return (
+      <button
+        {...{ disabled: props.disabled }}
+        onClick={props.onClick}
+        className={classNames("button", {
+          "button--confirm": props.confirm,
+          "button--danger": props.danger
+        })}
+      >
+        {props.children}
+      </button>
+  );
 }
