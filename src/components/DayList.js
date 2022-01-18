b04bc0572191d85413f49331a15d2f25773eae3e
meteora277 <justin.s.diaz@gmail.com>
2022-01-18 18:04:22 -0500

    add selected prop to component map

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -8,7 +8,8 @@ function DayList(props) {
       key={day.id} 
       name={day.name} 
       spots={day.spots}
-      setDay={(day) => console.log(day)}
+      setDay={(day) => props.setDay(day)}
+      selected={day.name === props.day}
       />
   })
 
