2ace943db840f832a254d02ec8bb0cbdbd6d26f1
meteora277 <justin.s.diaz@gmail.com>
2022-01-19 19:49:14 -0500

    create Appointment component

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Empty from './Empty';
+import Header from './Header';
+import Show from './Show';
 import './styles.scss'
 
-function Appointment({time}) {
+
+function Appointment(props) {
   return(
-    <article className='appointment'>{time ? 
-      `Appointment at ${time}`
-      : 
-      'No appointments' 
-    }
+    <article className='appointment'>
+
+      <Header time={props.time}/>
+      {props.interview ? 
+        <Show 
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        /> 
+        : 
+        <Empty/>}
+    
     </article>
   )
 }
