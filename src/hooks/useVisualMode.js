5d9bdf0cf9dfc2bea305515f7ccbf56a3c24eb56
meteora277 <justin.s.diaz@gmail.com>
2022-01-21 18:40:45 -0500

    create helper function for  appointment transitions

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+
+export const useVisualMode = (initialMode) => {
+  
+  const [mode, setMode] = useState(initialMode)
+  const [history, setHistory] = useState([initialMode])
+
+  const transition = (toMode, skipLast = false) => {
+    setMode(toMode)
+    if (skipLast){
+      setHistory(prev => {
+        let newPrev = [...prev]
+        newPrev.pop()
+        return [...newPrev, toMode]
+      })
+    } else {
+      setHistory(prev => [...prev, toMode])
+    }
+  }
+  const back = () => {
+    if (history.length > 0) {
+      setHistory(prev => {
+        let callStack = [...prev]
+        callStack.pop()
+        return callStack
+      })
+    }
+  }
+
+  useEffect(() => setMode(history[history.length - 1]), [history])
+
+  return {mode, setMode, transition, back}
+
+}
