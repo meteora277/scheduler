7e5ceb32a7566e3955781859dcbd5b71dd135c69
meteora277 <justin.s.diaz@gmail.com>
2022-01-21 19:16:46 -0500

    refactor Appointment to use userVisualMode hook

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,25 +1,29 @@
-import React, { Fragment } from 'react';
-import Empty from './Empty';
-import Header from './Header';
-import Show from './Show';
-import './styles.scss'
+import { useVisualMode } from "hooks/useVisualMode";
+import React from "react";
+import Empty from "./Empty";
+import Header from "./Header";
+import Show from "./Show";
+import "./styles.scss";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE"
 
 function Appointment(props) {
+  const { mode, setMode, transition, back} = useVisualMode(props.interview ? SHOW : EMPTY);
 
-  return(
-    <article className='appointment'>
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
 
-      <Header time={props.time}/>
-      {props.interview ? 
-        <Show 
+      {mode === "SHOW" && props.interview ? (
+        <Show
           student={props.interview.student}
           interviewer={props.interviewer}
-        /> 
-        : 
-        <Empty/>}
-    
+        />
+      ) : null}
+      {mode === "EMPTY" && <Empty onAdd={() => console.log("click")}/>}
     </article>
-  )
+  );
 }
-export default Appointment
+export default Appointment;
