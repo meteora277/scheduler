3b007ef24d7e998b8f20bad239ec512a8009e99d
meteora277 <justin.s.diaz@gmail.com>
2022-01-24 18:53:38 -0500

    create useApplicationData hook

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+const { useState, useEffect } = require("react");
+
+export default function useApplicationData() {
+  const [state, setState] = useState({
+    days: [],
+    day: "Monday",
+    appointments: [],
+    interviewers: {}
+  });
+
+  const setDay = (day) => {
+    setState((prev) => ({ ...prev, day }));
+  };
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
+    ]).then((all) => {
+      const [days, appointments, interviewers] = all;
+      setState((prev) => ({
+        ...prev,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      }));
+    });
+  }, []);
+
+  async function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios
+      .put(`http://localhost:8001/api/appointments/${id}`, { interview })
+      .then(() => setState((prev) => ({ ...prev, appointments })));
+  }
+
+  async function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios
+      .delete(`http://localhost:8001/api/appointments/${id}`)
+      .then(() => setState((prev) => ({ ...prev, appointments })));
+  }
+
+  return {state, setState, setDay, bookInterview, cancelInterview}
+}
