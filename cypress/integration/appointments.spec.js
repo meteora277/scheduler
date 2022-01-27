625376a6c54c79834c42695395d15984393bc880
meteora277 <justin.s.diaz@gmail.com>
2022-01-26 21:43:21 -0500

    run skipped tests

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -6,7 +6,7 @@ describe("Appointments", () => {
 
   })
 
-  xit ("Should book an interview", () => {
+  it ("Should book an interview", () => {
     cy.get("[alt=Add]").first().click()
     cy.get(".appointment__create-input").type("Lydia Miller-Jones")
     cy.get("[alt='Sylvia Palmer']").click()
@@ -15,7 +15,7 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Sylvia Palmer")
   })
 
-  xit("Should edit an interview", () => {
+  it("Should edit an interview", () => {
     cy.get(':nth-child(1) > .appointment__card [alt=Edit]').click({force: true})
     cy.get("[alt='Tori Malcolm']").click()
     cy.get("[data-testid=student-name-input").clear().type("Lydia Miller-Jones")
